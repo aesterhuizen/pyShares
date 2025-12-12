1ceaec11b7a3dd3773987c12d3451d6338d3ebd8
Anton Esterhuizen <aesterhu@gmail.com>
2025-12-12 11:41:18 -0500

    -V1.1.0

--- MainWindow.py
+++ MainWindow.py
@@ -49,7 +49,7 @@ class MainWindow(QMainWindow):
 
         
      
-        self.ver_string = "v1.0.43"
+        self.ver_string = "v1.1.0"
         self.icon_path = ''
         self.base_path = ''
         self.env_file = ''
